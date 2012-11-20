23eccd10f2121caf0e2e6749f117b4b2cb33e495
Jeremy Darby <jdarby@umich.edu>
2012-11-20 17:46:55 -0500

    Here's untested code to actually access the MBus XML Feed. Actually, you can't even get to it yet rofl

--- src/com/eecs/mnav/StartActivity.java
+++ src/com/eecs/mnav/StartActivity.java
@@ -1,9 +1,18 @@
 package com.eecs.mnav;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParserException;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences.Editor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
@@ -20,6 +29,17 @@ public class StartActivity extends Activity {
 
 	public String address = "";
 
+	public static final String WIFI = "Wi-Fi";
+	public static final String ANY = "Any";
+	private static final String URL = "http://mbus.pts.umich.edu/shared/public_feed.xml";
+
+	// Whether there is a Wi-Fi connection.
+	private static boolean wifiConnected = false; 
+	// Whether there is a mobile connection.
+	private static boolean mobileConnected = false;
+	// Whether the display should be refreshed.
+	public static boolean refreshDisplay = true; 
+	public static String sPref = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -40,7 +60,7 @@ public class StartActivity extends Activity {
 				Editor editor = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
 				editor.putString("DESTADDR", address);
 				editor.commit();
-				
+
 				//hide the soft keyboard
 				InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
 				imm.hideSoftInputFromWindow(address_box.getWindowToken(), 0);
@@ -58,5 +78,87 @@ public class StartActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_start, menu);
 		return true;
+	}
+
+
+	// Uses AsyncTask to download the XML feed from mbus.pts.umich.edu.
+	public void loadPage() {  
+
+		if((sPref.equals(ANY)) && (wifiConnected || mobileConnected)) {
+			new DownloadXmlTask().execute(URL);
+		}
+		else if ((sPref.equals(WIFI)) && (wifiConnected)) {
+			new DownloadXmlTask().execute(URL);
+		} else {
+			// show error
+		}  
+	}
+
+	// Implementation of AsyncTask used to download XML feed from mbus.pts.umich.edu.
+	private class DownloadXmlTask extends AsyncTask<String, Void, String> {
+		@Override
+		protected String doInBackground(String... urls) {
+			try {
+				return loadXmlFromNetwork(urls[0]);
+			} catch (IOException e) {
+				return "Connection error";
+			} catch (XmlPullParserException e) {
+				return "XML error";
+			}
+		}
+
+		@Override
+		protected void onPostExecute(String result) {  
+			/*
+			setContentView(R.layout.main);
+			// Displays the HTML string in the UI via a WebView
+			WebView myWebView = (WebView) findViewById(R.id.webview);
+			myWebView.loadData(result, "text/html", null);
+			*/
+		}
+	}
+
+	// Uploads XML from mbus.pts.umich.edu and parses it
+	private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+		InputStream stream = null;
+		// Instantiate the parser
+		MbusPublicFeedXmlParser mbusPublicFeedXmlParser = new MbusPublicFeedXmlParser();
+		List<MbusPublicFeedXmlParser.Route> routes = null;
+
+		try {
+			stream = downloadUrl(urlString);        
+			routes = mbusPublicFeedXmlParser.parse(stream);
+			// Makes sure that the InputStream is closed after the app is
+			// finished using it.
+		} finally {
+			if (stream != null) {
+				stream.close();
+			} 
+		}
+
+		// StackOverflowXmlParser returns a List (called "entries") of Entry objects.
+		// Each Entry object represents a single post in the XML feed.
+		// This section processes the entries list to combine each entry with HTML markup.
+		// Each entry is displayed in the UI as a link that optionally includes
+		// a text summary.
+		for (MbusPublicFeedXmlParser.Route route : routes) {
+			
+		}
+		return "Something";
+	}
+
+	// Given a string representation of a URL, sets up a connection and gets
+	// an input stream.
+	private InputStream downloadUrl(String urlString) throws IOException {
+		URL url = new URL(urlString);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setReadTimeout(10000 /* milliseconds */);
+		conn.setConnectTimeout(15000 /* milliseconds */);
+		conn.setRequestMethod("GET");
+		conn.setDoInput(true);
+		// Starts the query
+		conn.connect();
+		InputStream stream = conn.getInputStream();
+		return stream;
 	}
 }
